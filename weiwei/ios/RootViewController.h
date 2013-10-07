3e937e03cec7e7969218aa197cd0d142834c2cae
konghuari <konghuari@163.com>
2013-10-07 17:33:15 +0800

    Initial Commit

---
+++ weiwei/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  weiweiAppController.h
+//  weiwei
+//
+//  Created by konghuari on 13-10-7.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
